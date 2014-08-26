8ee4bae52b7af998105389953387f3d725a9a7ef
Érico Andrei <erico@simplesconsultoria.com.br>
2014-08-26 11:34:23 -0300

    Remove spaces

--- src/collective/transmogrifier/sections/condition.py
+++ src/collective/transmogrifier/sections/condition.py
@@ -9,12 +9,12 @@ from zope.interface import implements
 class ConditionSection(object):
     classProvides(ISectionBlueprint)
     implements(ISection)
-    
+
     def __init__(self, transmogrifier, name, options, previous):
         condition = options['condition']
         self.condition = Condition(condition, transmogrifier, name, options)
         self.previous = previous
-    
+
     def __iter__(self):
         for item in self.previous:
             if self.condition(item):
