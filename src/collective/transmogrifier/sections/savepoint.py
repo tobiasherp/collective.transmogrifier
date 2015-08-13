0a69ecfed9aff665e892455738d437ab52099acb
Tobias Herp <tobias.herp@stein.de>
2015-08-13 09:36:05 +0200

    Counting enabled for savepoint section
    Counting occurs *before* yield because this is the moment when the
    method looses control; this way it is more likely the counting to be
    accurate.

--- src/collective/transmogrifier/sections/savepoint.py
+++ src/collective/transmogrifier/sections/savepoint.py
@@ -11,11 +11,16 @@ class SavepointSection(object):
     def __init__(self, transmogrifier, name, options, previous):
         self.every = int(options.get('every', 1000))
         self.previous = previous
+        self.count = transmogrifier.create_itemcounter(name)
 
     def __iter__(self):
+        count_ = self.count
         count = 0
         for item in self.previous:
+            count_('got')
             count = (count + 1) % self.every
             if count == 0:
+                count_('commits')
                 transaction.savepoint(optimistic=True)
+            count_('forwarded')
             yield item
